c4285384144014d06eb4ccff510c0bdfdeedfb26
kuanxiii <kuanxiii@163.com>
2024-01-18 00:44:59 +0800

    init

---
+++ account-server/src/test/java/me/account/account/server/AccountServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.account.account.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
